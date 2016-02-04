9f86be512612284fb1d2ef3d2d35e2c6dd62f1c9
Clayton Webb <claywebb7@gmail.com>
2016-02-03 23:59:45 -0500

    Fixed a big where capitalized file extensions wouldn't add, again

--- Screen.py
+++ Screen.py
@@ -2,7 +2,7 @@ import pygame
 import glob
 import sys
 
-__DIRPATH = "/home/pi/Images" # Directory of Images
+__DIRPATH = "/home/pi/Images/" # Directory of Images
 __WIDTH, __HEIGHT = 1920, 1080 # Screen resolution for scaling purposes
 __DELAY = 7000 # Time in Milliseconds (1000 ms = 1 second)
 
@@ -11,11 +11,13 @@ screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 done = False
 
 
-imagePaths = glob.glob(__DIRPATH+"*.png") + glob.glob(__DIRPATH+"*.jpg") + glob.glob(__DIRPATH+"*.JPG")
+imagePaths = glob.glob(__DIRPATH+"*.png") + glob.glob(__DIRPATH+"*.PNG") + glob.glob(__DIRPATH+"*.jpg") + glob.glob(__DIRPATH+"*.JPG")
 
 print(imagePaths[:])
 
 #del imagePaths[-1]
+
+imagePaths.sort()
 
 images = []
 
