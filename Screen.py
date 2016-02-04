2ce127f2b1eb003628da0781de23117cdb05ce88
Clayton Webb <claywebb7@gmail.com>
2016-02-03 22:05:50 -0500

    Initial commit

---
+++ Screen.py
@@ -0,0 +1,41 @@
+import pygame
+import glob
+import sys
+
+__DIRPATH = "/Users/claywebb/Desktop/images/" # Directory of Images
+__WIDTH, __HEIGHT = 1920, 1080 # Screen resolution for scaling purposes
+__DELAY = 7000 # Time in Milliseconds (1000 ms = 1 second)
+
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+done = False
+
+
+imagePaths = glob.glob(__DIRPATH+"*.png") + glob.glob(__DIRPATH+"*.jpg")
+
+print(imagePaths[:])
+
+del imagePaths[-1]
+
+images = []
+
+for file in imagePaths:
+    if file != "":
+        images.append(pygame.image.load(file))
+
+while not done:
+
+    for i in images:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                sys.exit()
+
+        screen.fill((0,0,0))
+        screen.blit(pygame.transform.scale(i, (__WIDTH, __HEIGHT)), (0,0))
+        pygame.display.flip()
+        pygame.time.delay(__DELAY)
+
